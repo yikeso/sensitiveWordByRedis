bf05879639ecf3b36db0acc62ffebddf6d7e6161
kakasun <hejiademsn@live.cn>
2017-07-11 08:36:24 +0800

    优化redis储存结构，节约空间，提高性能。

--- src/main/java/com/china/example/config/RedisConfig.java
+++ src/main/java/com/china/example/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.china.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
@@ -42,64 +43,65 @@ public class RedisConfig {
      * @return
      */
     @Bean
+    @Primary
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         initDomainRedisTemplate(redisTemplate, redisConnectionFactory);
         return redisTemplate;
     }
 
-    /**
+/*    *//*
      * 实例化 HashOperations 对象,可以使用 Hash 类型操作
      *
      * @param redisTemplate
      * @return
-     */
+     *//*
     @Bean
     public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
         return redisTemplate.opsForHash();
     }
 
-    /**
+    *//*
      * 实例化 ValueOperations 对象,可以使用 String 操作
      *
      * @param redisTemplate
      * @return
-     */
+     *//*
     @Bean
     public ValueOperations<String, Object> valueOperations(RedisTemplate<String, Object> redisTemplate) {
         return redisTemplate.opsForValue();
     }
 
-    /**
+    *//*
      * 实例化 ListOperations 对象,可以使用 List 操作
      *
      * @param redisTemplate
      * @return
-     */
+     *//*
     @Bean
     public ListOperations<String, Object> listOperations(RedisTemplate<String, Object> redisTemplate) {
         return redisTemplate.opsForList();
     }
 
-    /**
+    *//*
      * 实例化 SetOperations 对象,可以使用 Set 操作
      *
      * @param redisTemplate
      * @return
-     */
+     *//*
     @Bean
     public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate) {
         return redisTemplate.opsForSet();
     }
 
-    /**
+    *//*
      * 实例化 ZSetOperations 对象,可以使用 ZSet 操作
      *
      * @param redisTemplate
      * @return
-     */
+     *//*
     @Bean
     public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
         return redisTemplate.opsForZSet();
-    }
+    }*/
 }
